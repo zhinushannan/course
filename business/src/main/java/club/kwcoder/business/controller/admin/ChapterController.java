36681ade860b957cbd3261f5c54c753a149f7d55
zhinushannan <1377875184@qq.com>
2021-11-16 19:06:35 +0800

    1、 添加business网关

--- business/src/main/java/club/kwcoder/business/controller/admin/ChapterController.java
+++ business/src/main/java/club/kwcoder/business/controller/admin/ChapterController.java
@@ -1,10 +1,8 @@
-package club.kwcoder.business.controller;
+package club.kwcoder.business.controller.admin;
 
 
 import club.kwcoder.server.dataobject.ChapterDO;
-import club.kwcoder.server.dataobject.TestDO;
 import club.kwcoder.server.service.ChapterService;
-import club.kwcoder.server.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +15,8 @@ import java.util.List;
  * @description:
  */
 @RestController
-public class TestController {
+@RequestMapping("/admin")
+public class ChapterController {
 
     @Autowired
     private ChapterService chapterService;
