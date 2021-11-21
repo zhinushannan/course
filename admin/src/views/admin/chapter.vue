dc17ee154becce88119536eaa0f887a52f3971e0
zhinushannan <1377875184@qq.com>
2021-11-21 13:47:21 +0800

    1、 制作toast组件，内部用sweetalert2实现

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -155,11 +155,7 @@ export default {
             let resp = response.data
             if (resp.success) {
               _this.list(1)
-              Swal.fire(
-                  '删除成功！',
-                  '删除成功！',
-                  'success'
-              )
+              toast.success("删除成功！")
             }
           })
         }})
@@ -185,6 +181,7 @@ export default {
         if (resp.success) {
           $("#form-modal").modal("hide")
           _this.list(1)
+          toast.success("保存成功！")
         }
       })
     }
