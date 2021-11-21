0f83e5838004f235c75c998cafa558e88379d941
zhinushannan <1377875184@qq.com>
2021-11-21 13:36:24 +0800

    1、 集成sweetalert2，删除时弹出确认框

---
+++ doc/sql/chapter.sql
@@ -0,0 +1,24 @@
+use courseimooc;
+
+delete from chapter where id like '%';
+
+insert into chapter (`id`, `course_id`, `name`) values ('00000001', '00000001', '测试大章01');
+insert into chapter (`id`, `course_id`, `name`) values ('00000002', '00000002', '测试大章02');
+insert into chapter (`id`, `course_id`, `name`) values ('00000003', '00000003', '测试大章03');
+insert into chapter (`id`, `course_id`, `name`) values ('00000004', '00000004', '测试大章04');
+insert into chapter (`id`, `course_id`, `name`) values ('00000005', '00000005', '测试大章05');
+insert into chapter (`id`, `course_id`, `name`) values ('00000006', '00000006', '测试大章06');
+insert into chapter (`id`, `course_id`, `name`) values ('00000007', '00000007', '测试大章07');
+insert into chapter (`id`, `course_id`, `name`) values ('00000008', '00000008', '测试大章08');
+insert into chapter (`id`, `course_id`, `name`) values ('00000009', '00000009', '测试大章09');
+insert into chapter (`id`, `course_id`, `name`) values ('00000010', '00000010', '测试大章10');
+insert into chapter (`id`, `course_id`, `name`) values ('00000011', '00000011', '测试大章11');
+insert into chapter (`id`, `course_id`, `name`) values ('00000012', '00000012', '测试大章12');
+insert into chapter (`id`, `course_id`, `name`) values ('00000013', '00000013', '测试大章13');
+insert into chapter (`id`, `course_id`, `name`) values ('00000014', '00000014', '测试大章14');
+insert into chapter (`id`, `course_id`, `name`) values ('00000015', '00000015', '测试大章15');
+insert into chapter (`id`, `course_id`, `name`) values ('00000016', '00000016', '测试大章16');
+insert into chapter (`id`, `course_id`, `name`) values ('00000017', '00000017', '测试大章17');
+insert into chapter (`id`, `course_id`, `name`) values ('00000018', '00000018', '测试大章18');
+insert into chapter (`id`, `course_id`, `name`) values ('00000019', '00000019', '测试大章19');
+insert into chapter (`id`, `course_id`, `name`) values ('00000020', '00000020', '测试大章20');
