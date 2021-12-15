3df3cf7274db63c349549c46169860a4c8595200
zhinushannan <1377875184@qq.com>
2021-12-16 00:10:26 +0800

    1、 SessionStorage 的 key 改成常量

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -140,7 +140,7 @@ export default {
   mounted() {
     let _this = this;
     _this.$refs.pagination.size = 5;
-    let course = SessionStorage.get("course") || {};
+    let course = SessionStorage.get(SESSION_KEY_COURSE) || {};
     if (Tool.isEmpty(course)) {
       _this.$router.push("/welcome");
     }
@@ -212,7 +212,7 @@ export default {
     },
     toSection(chapter) {
       let _this = this;
-      SessionStorage.set("chapter", chapter);
+      SessionStorage.set(SESSION_KEY_CHAPTER, chapter);
       _this.$router.push("/business/section")
     }
   }
--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -377,7 +377,7 @@ export default {
      */
     toChapter(course) {
       let _this = this;
-      SessionStorage.set("course", course)
+      SessionStorage.set(SESSION_KEY_COURSE, course)
       _this.$router.push("/business/chapter")
     },
     /**
