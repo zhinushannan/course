cf19cfd03249325dfce414011f02e5d4016c7d5b
zhinushannan <1377875184@qq.com>
2021-12-18 16:55:20 +0800

    1、 文件上传功能开发：去掉文件管理的新增，修改，删除功能

--- file/src/main/java/club/kwcoder/file/controller/admin/FileController.java
+++ file/src/main/java/club/kwcoder/file/controller/admin/FileController.java
@@ -30,27 +30,4 @@ public class FileController {
         return ResultBean.getSuccess("查询成功", pageDto);
     }
 
-    /**
-     * 保存，id有值时更新，无值时新增
-     */
-    @PostMapping("/save")
-    public ResultBean<FileDTO> save(@RequestBody FileDTO fileDto) {
-        // 保存校验
-        ValidatorUtil.require(fileDto.getPath(), "相对路径");
-        ValidatorUtil.length(fileDto.getPath(), "相对路径", 1, 100);
-        ValidatorUtil.length(fileDto.getName(), "文件名", 1, 100);
-        ValidatorUtil.length(fileDto.getSuffix(), "后缀", 1, 10);
-        ValidatorUtil.length(fileDto.getKey(), "文件标识", 1, 32);
-
-        return ResultBean.getSuccess("新增成功", fileDto);
-    }
-
-    /**
-     * 删除
-     */
-    @DeleteMapping("/delete/{id}")
-    public ResultBean<String> delete(@PathVariable String id) {
-        fileService.delete(id);
-        return ResultBean.getSuccess("删除成功", null);
-    }
 }
