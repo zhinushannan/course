3c2918692e345827e341857b1e718d09547d6090
zhinushannan <1377875184@qq.com>
2021-12-24 19:38:02 +0800

    1、 大文件断点续传与极速秒传：重构文件上传代码，将上传功能提取成一个单独的upload方法，实现连续上传多个分片功能

--- admin/src/components/big-file.vue
+++ admin/src/components/big-file.vue
@@ -37,8 +37,6 @@
     methods: {
       uploadFile() {
         let _this = this
-        let formData = new window.FormData()
-        // key: "file" 必须和后端controller参数名保持一致
         let file = _this.$refs.file.files[0]
 
         let key = hex_md5(file)
@@ -66,49 +64,62 @@
         // 文件分片
         let shardSize = 1 * 1204 * 1024; // 以2MB为一个分片
         let shardIndex = 0; // 分片索引
-        let start = (shardIndex - 1) * shardSize; // 当前分片起始位置
-        let end = Math.min(start + shardSize, file.size); // 当前分片结束位置
-        let fileShard = file.slice(start, end)
-
+        let fileShard = this.getFileShard(shardIndex, shardSize);
         let size = file.size;
         let shardTotal = Math.ceil(size / shardSize);
 
-
-
-        // 将文件转为base64进行传输
-        let fileReader = new FileReader()
-        fileReader.onload = function (e) {
-          let base64 = e.target.result
-          
-          let param = {
-            "shard": base64,
-            "shardIndex": shardIndex,
-            "shardSize": shardSize,
-            "shardTotal": shardTotal,
-            "use": _this.use,
-            "name": file.name,
-            "suffix": suffix,
-            "size": size,
-            "key": key62,
-          }
-
-          Loading.show()
-          _this.$ajax.post(process.env.VUE_APP_SERVER + "/file/admin/upload", param).then((response) => {
-            Loading.hide()
-            let resp = response.data
-            _this.afterUpload(resp)
-            $("#" + _this.inputId + "-input").val("")
-          })
-          
+        let param = {
+          "shardIndex": shardIndex,
+          "shardSize": shardSize,
+          "shardTotal": shardTotal,
+          "use": _this.use,
+          "name": file.name,
+          "suffix": suffix,
+          "size": size,
+          "key": key62,
         }
-        fileReader.readAsDataURL(fileShard);
-
+        _this.upload(param);
       },
 
       selectFile() {
         let _this = this
         $("#" + _this.inputId + "-input").trigger("click")
-      }
+      },
+
+      upload(param) {
+        let _this = this
+        // 将文件转为base64进行传输
+        let shardIndex = param["shardIndex"]
+        let shardTotal = param["shardTotal"]
+        let shardSize = param["shardSize"]
+        let fileReader = new FileReader()
+        fileReader.onload = function (e) {
+          param["shard"] = e.target.result
+
+          Loading.show()
+          _this.$ajax.post(process.env.VUE_APP_SERVER + "/file/admin/upload", param).then((response) => {
+            Loading.hide()
+            let resp = response.data
+            if (shardIndex < shardTotal) {
+              // 上传下一个分片
+              param["shardIndex"] = param["shardIndex"] + 1
+              _this.upload(param)
+            } else {
+              _this.afterUpload(resp)
+            }
+            $("#" + _this.inputId + "-input").val("")
+          })
+        }
+        let fileShard = _this.getFileShard(shardIndex, shardSize)
+        fileReader.readAsDataURL(fileShard);
+      },
+      getFileShard(shardIndex, shardSize) {
+        let _this = this
+        let file = _this.$refs.file.files[0]
+        let start = (shardIndex - 1) * shardSize; // 当前分片起始位置
+        let end = Math.min(start + shardSize, file.size); // 当前分片结束位置
+        return file.slice(start, end);
+      },
     }
   }
 </script>
