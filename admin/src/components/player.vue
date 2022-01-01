0243be64e19143cac4e9e25e98e4017f156179a0
zhinushannan <1377875184@qq.com>
2022-01-01 15:34:37 +0800

    1、视频加密与授权播放：获取vod授权并授权播放，需要在存储管理中吧权限设置成公共读

--- admin/src/components/player.vue
+++ admin/src/components/player.vue
@@ -1,50 +1,77 @@
 <template>
   <div v-bind:id="playerId">
-
+    <!--    <div  class="prism-player" id="J_prismPlayer"></div>-->
   </div>
 </template>
-
 <script>
 export default {
-  name: "player",
+  name: 'player',
   props: {
     playerId: {
       default: "player-div"
-    }
+    },
   },
-  data() {
+  data: function () {
     return {
-      aliPlayer: {},
+      aliPlayer: {}, // 播放器实例
     }
   },
   methods: {
     playUrl(url) {
-      let _this = this
-      console.log("开始播放：", url)
+      let _this = this;
+      console.log("开始播放:", url);
 
-      // 如果已经有播放器了，则将播放器div删除
+      //如果已经有播放器了，则将播放器div删除
       if (_this.aliPlayer) {
         _this.aliPlayer = null;
-        $("#J_prismPlayer").remove()
+        $("#" + _this.playerId + '-player').remove();
       }
 
       // 初始化播放器
-      $("#" + _this.playerId).append("<div class='prism-player' id='J_prismPlayer'></div>")
+      $("#" + _this.playerId).append("<div class=\"prism-player\" id=\"" + _this.playerId + "-player\"></div>");
       _this.aliPlayer = new Aliplayer({
-        id: "J_prismPlayer",
-        width: "100%",
+        id: _this.playerId + '-player',
+        width: '100%',
         autoplay: false,
         source: url,
-        cover: "http://liveroom-img.oss-cn-qingdao.aliyuncs.com/logo.png"
+        cover: 'http://liveroom-img.oss-cn-qingdao.aliyuncs.com/logo.png',
       }, function (player) {
-        console.log("播放器创建好了。")
+        console.log('播放器创建好了。')
+      });
+    },
+
+    playVod (vod) {
+      let _this = this;
+      Loading.show();
+      _this.$ajax.get(process.env.VUE_APP_SERVER + '/file/admin/get-auth/' + vod).then((response)=>{
+        Loading.hide();
+        let resp = response.data;
+        if (resp.success) {
+          //如果已经有播放器了，则将播放器div删除
+          if (_this.aliPlayer) {
+            _this.aliPlayer = null;
+            $("#" + _this.playerId + '-player').remove();
+          }
+
+          // 初始化播放器
+          $("#" + _this.playerId).append("<div class=\"prism-player\" id=\"" + _this.playerId + "-player\"></div>");
+          _this.aliPlayer = new Aliplayer({
+            id: _this.playerId + '-player',
+            width: '100%',
+            autoplay: false,
+            vid: vod,
+            playauth: resp.data,
+            cover: 'http://liveroom-img.oss-cn-qingdao.aliyuncs.com/logo.png',
+            encryptType:1, //当播放私有加密流时需要设置。
+          },function(player){
+            console.log('播放器创建好了。')
+          });
+        } else {
+          Toast.warning('播放错误。')
+        }
       })
 
     }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -31,11 +31,10 @@
         <th>标题</th>
         <th>课程</th>
         <th>大章</th>
-        <th>视频</th>
+        <th>VOD</th>
         <th>时长</th>
         <th>收费</th>
         <th>顺序</th>
-        <th>vod</th>
         <th>操作</th>
       </tr>
       </thead>
@@ -46,13 +45,15 @@
         <td>{{ section.title }}</td>
         <td>{{ section.courseId }}</td>
         <td>{{ section.chapterId }}</td>
-        <td>{{ section.video }}</td>
+        <td>{{ section.vod }}</td>
         <td>{{ section.time | formatSecond }}</td>
         <td>{{ section.charge }}</td>
         <td>{{ section.sort }}</td>
-        <td>{{ section.vod }}</td>
         <td>
           <div class="hidden-sm hidden-xs btn-group">
+            <button v-on:click="play(section)" class="btn btn-xs btn-info">
+              <i class="ace-icon fa fa-video-camera bigger-120"></i>
+            </button>
             <button class="btn btn-xs btn-info" v-on:click="edit(section)">
               <i class="ace-icon fa fa-pencil bigger-120"></i>
             </button>
@@ -104,7 +105,7 @@
                       v-bind:use="FILE_USE.COURSE.key"></Vod>
                   <div v-show="section.video" class="row">
                     <div class="col-md-9">
-                      <player ref="player"></player>
+                      <player v-bind:player-id="'form-player-div'" ref="player"></player>
                       <video v-show="section.video" id="video" controls="controls" v-bind:src="section.video" class="hidden"/>
                     </div>
                   </div>
@@ -157,6 +158,8 @@
         </div><!-- /.modal-content -->
       </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
+
+    <modal-player ref="modalPlayer"></modal-player>
   </div>
 </template>
 
@@ -165,9 +168,10 @@ import Pagination from "../../components/pagination";
 import BigFile from "../../components/big-file";
 import Vod from "@/components/vod";
 import Player from "@/components/player";
+import ModalPlayer from "@/components/modal-player";
 
 export default {
-  components: {Player, Vod, Pagination, BigFile},
+  components: {ModalPlayer, Player, Vod, Pagination, BigFile},
   name: "business-section",
   data: function () {
     return {
@@ -303,6 +307,15 @@ export default {
       let _this = this;
       let ele = document.getElementById("video")
       _this.section.time = parseInt(ele.duration, 1000)
+    },
+
+    /**
+     * 播放视频
+     * @param section
+     */
+    play(section) {
+      let _this = this
+      _this.$refs["modalPlayer"].playVod(section.vod)
     }
 
   }
